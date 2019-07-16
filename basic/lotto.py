1bff165af3629268dbc801b3d020fcd9c5edf004
wognsdl1126 <wognsdl1126@gmail.com>
2019-07-16 19:16:59 +0900

    1

--- basic/lotto.py
+++ basic/lotto.py
@@ -5,8 +5,7 @@ num = random.randint(1, 45)
 
 for i in range(0,5):
     for j in range(0,6):        
-        while num in arr:
-            print(num)          
+        while num in arr:                
             num = random.randint(1, 45)            
         arr[j] = num    
     arr.sort()
