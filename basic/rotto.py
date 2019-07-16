a74096cbe68738739cc9c39c0cd9ded144c768dd
wognsdl1126 <wognsdl1126@gmail.com>
2019-07-16 19:14:20 +0900

    Assignment

--- basic/rotto.py
+++ basic/rotto.py
@@ -1,14 +1,14 @@
 import random
-arr = [0,0,0,0,0,0]
 
+arr = [0,0,0,0,0,0]
+num = random.randint(1, 45)
 
 for i in range(0,5):
-    for j in range(0,6):
-        while arr[j] != arr [j-1]:
-            num = random.randint(1, 45)
-            break
-        num = random.randint(1, 45)
-        arr[j] = num
+    for j in range(0,6):        
+        while num in arr:
+            print(num)          
+            num = random.randint(1, 45)            
+        arr[j] = num    
     arr.sort()
     print(arr)
     
